78af68b8bf298dd14abf2034e2e644ce3b31373a
MatheMaticSu <549603781@qq.com>
2021-05-13 16:20:43 +0800

    5.13

--- sortdemo.py
+++ sortdemo.py
@@ -1,3 +1,5 @@
+lis = [4,5,6,1,3,2]
+
 '''
 冒泡排序
 '''
@@ -19,7 +21,50 @@ class Bubble():
         temp = s[i]
         s[i] = s[j]
         s[j] = temp
-
-lis = [4,5,6,1,3,2]
-Bubble.sorte(lis)
+# Bubble.sorte(lis)
+# print(lis)
+'''
+选择排序
+'''
+class Selects():
+    def compare(a, b):
+        if a > b:
+            return True
+    
+    def exch(s, i ,j):
+        temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+    
+    def sorte(s):
+        l = len(s)
+        for i in range(0,l-2):
+            minindex = i
+            for j in range(i+1,l):
+                if Selects.compare(s[minindex], s[j]):
+                    minindex = j
+            Selects.exch(s, i, minindex)
+# Selects.sorte(lis)
+# print(lis)
+'''
+插入排序
+'''
+class Insertion():
+    def compare(a, b):
+        if a > b:
+            return True
+    def exch(s, i, j):
+        temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+    def sorte(s):
+        for i in range(1,len(s)):
+            j = i 
+            while j>0:
+                if Insertion.compare(s[j-1],s[j]):
+                    Insertion.exch(s,j-1,j)
+                    j -= 1
+                else:
+                    break
+Insertion.sorte(lis)
 print(lis)
